4c57f9b4e8fd21c5c20571faf7eed84fd46c3fe9
VeredanLord <remy.melquiond@gmail.com>
2017-02-26 05:11:25 +0100

    Menu + angular2 material lite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
+
+import { MdlModule } from "angular2-mdl";
 
 import { AppComponent }  from './app.component';
 
+import 'hammerjs';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MaterialModule, MdlModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -22,6 +22,10 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      'angular2-mdl': 'npm:angular2-mdl/bundle/angular2-mdl.js',
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+      'hammerjs': 'npm:hammerjs/hammer.js',
+      
 
       // other libraries
       'rxjs':                      'npm:rxjs',
